d5de318817779c81f7ff40c908fb46b47c958d7f
Pradumna <pradumnabajoria@gmail.com>
2021-05-27 20:44:13 +0530

    basic monster-rolodex created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react"
 import './App.css';
+import CardList from "./components/cardList/CardList"
+import SearchBox from "./components/searchBox/SearchBox"
 
 function App() {
+
+  const [monsters, setMonsters] = useState(
+    [ ]
+  )
+  const [search, setSearch] = useState("")
+
+  //const url = "https://jsonplaceholder.typicode.com/users"
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => setMonsters(
+      users
+    ))
+    
+  }, [])
+
+  const filteredMonsters = monsters.filter(monster => 
+    monster.name.toLowerCase().includes(search.toLowerCase())
+    )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBox 
+        placeholder='search monsters'
+        handleChange = {e => setSearch(e.target.value)}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
---
+++ src/components/cardList/CardList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./cardList.css"
+import {Card} from "../card/Card"
+
+const CardList = props => {
+    //console.log("hey",props)
+    return(<div className="card-list">
+        {
+          props.monsters.map(monster => (
+            <Card key={monster.id} monster={monster}/>
+          ))
+        }
+    </div>)
+}
+
+export default CardList
